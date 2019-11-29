71f46349f5df4d5da1f61d5380219bf5f9373b20
Janith Anoma <janith@elegantmedia.lk>
2019-11-30 01:29:04 +0530

    profile + profile edit

--- app/src/main/java/com/ewind/newsapi/data/local/database/UserDao.kt
+++ app/src/main/java/com/ewind/newsapi/data/local/database/UserDao.kt
@@ -5,15 +5,17 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ewind.newsapi.data.local.model.UserDB
+import io.reactivex.Completable
+import io.reactivex.Observable
 
 @Dao
 interface UserDao {
 
     @Query("SELECT * FROM userdb")
-    abstract fun getAll(): List<UserDB>
+    abstract fun getAll(): Observable<List<UserDB>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract fun insertUser(userDB: UserDB)
+    abstract fun insertUser(userDB: UserDB): Completable
 
     @Query("DELETE FROM userdb")
     abstract fun delete()
--- app/src/main/java/com/ewind/newsapi/ki/Modules.kt
+++ app/src/main/java/com/ewind/newsapi/ki/Modules.kt
@@ -2,10 +2,13 @@ package com.ewind.newsapi.ki
 
 import com.ewind.newsapi.data.repository.NewsRepositoryImpl
 import com.ewind.newsapi.data.repository.PreferenceRepositoryImpl
+import com.ewind.newsapi.data.repository.ProfileRespositoryImpl
 import com.ewind.newsapi.domain.repository.NewsRepository
 import com.ewind.newsapi.domain.repository.PreferenceRepository
+import com.ewind.newsapi.domain.repository.ProfileRepository
 import com.ewind.newsapi.domain.usecase.NewsUseCase
 import com.ewind.newsapi.domain.usecase.PreferenceUseCase
+import com.ewind.newsapi.domain.usecase.ProfileUserCase
 import com.ewind.newsapi.ui.main.home.HomeViewModel
 import com.ewind.newsapi.ui.main.news.NewsViewModel
 import com.ewind.newsapi.ui.main.profile.ProfileViewModel
@@ -30,13 +33,14 @@ private val loadFeature by lazy {
 val viewModelModule: Module = module {
     viewModel { HomeViewModel() }
     viewModel { NewsViewModel(newsUseCase = get(), preferenceUseCase = get()) }
-    viewModel { ProfileViewModel() }
+    viewModel { ProfileViewModel(profileUserCase = get()) }
     viewModel { TopNewsViewModel(topNewsUseCase = get()) }
 }
 
 val useCaseModule: Module = module {
     factory { NewsUseCase(newsRepository = get()) }
     factory { PreferenceUseCase(preferenceRepository = get()) }
+    factory { ProfileUserCase(profileRepository = get()) }
 }
 
 val repositoryModule: Module = module {
@@ -47,6 +51,11 @@ val repositoryModule: Module = module {
     }
     single<PreferenceRepository> {
         PreferenceRepositoryImpl(
+            databaseClient = get()
+        )
+    }
+    single<ProfileRepository> {
+        ProfileRespositoryImpl(
             databaseClient = get()
         )
     }
