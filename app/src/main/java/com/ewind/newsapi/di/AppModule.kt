62e8bd712f2c32411a15927d8d3bf9a162192eaf
Janith Anoma <janith@elegantmedia.lk>
2020-07-10 09:11:37 +0530

    add retrofit cache

--- app/src/main/java/com/ewind/newsapi/di/AppModule.kt
+++ app/src/main/java/com/ewind/newsapi/di/AppModule.kt
@@ -6,6 +6,7 @@ import com.ewind.newsapi.data.source.remote.apis.NewsApi
 import com.ewind.newsapi.util.network.SupportInterceptor
 import com.ewind.newsapi.util.network.createNetworkClient
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -14,18 +15,17 @@ private val tokenAuthenticator = SupportInterceptor(BuildConfig.API_KEY)
 
 val networkModule: Module = module {
     single { tokenAuthenticator }
-    single { NEWS_API }
+    single {
+        createNetworkClient(
+            androidContext(),
+            BuildConfig.API_URL,
+            BuildConfig.DEBUG,
+            tokenAuthenticator
+        )
+    }
+    single { get<Retrofit>().create(NewsApi::class.java) }
 }
 
 val dataBaseModule: Module = module {
     single { DatabaseClient(androidApplication()) }
 }
-
-val retrofit: Retrofit =
-    createNetworkClient(
-        BuildConfig.API_URL,
-        BuildConfig.DEBUG,
-        tokenAuthenticator
-    )
-
-val NEWS_API: NewsApi = retrofit.create(NewsApi::class.java)
